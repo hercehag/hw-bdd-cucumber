562caa5cabba8a7ac16d39bc72f8508daa849e99
Beatriz <hercehag@grinnell.edu>
2017-09-29 02:57:20 +0000

    HW3 Part II commit

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -30,10 +30,13 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  ratings = rating_list.split ","
+  ratings.each { |r| step "I #{uncheck}check \"ratings_#{r}\""}
+  #fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  page.all('table#movies tr').count.should == (Movie.count + 1)
+  #fail "Unimplemented"
 end
